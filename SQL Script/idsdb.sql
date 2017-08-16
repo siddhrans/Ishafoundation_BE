15b3b325276cc452a8a09a422a155d98b462f49e
Siddhrans <gururaj.n@siddhrans.com>
2017-08-16 13:52:29 +0530

    upload and download document

--- SQL Script/idsdb.sql
+++ SQL Script/idsdb.sql
@@ -1,35 +1,28 @@
-CREATE DATABASE `idsdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-CREATE TABLE `donardetails` (
-  `Donor_Details_ID` int(11) NOT NULL,
+CREATE DATABASE `idsdb`;
+ 
+CREATE TABLE `donordetails` (
+   `Donor_Details_ID` int(11) NOT NULL AUTO_INCREMENT ,
   `Donor_Phone_Num` varchar(45) NOT NULL,
   `Donor_Name` varchar(45) NOT NULL,
   `Donor_Email_id` varchar(45) NOT NULL,
-  `Donor_Reference_Name_1` varchar(45) DEFAULT NULL,
-  `Donor_Reference_Name_2` varchar(45) DEFAULT NULL,
-  `Donor_Status` varchar(45) DEFAULT NULL,
+   `Donor_Status` varchar(45) DEFAULT NULL,
   `Donor_Region` varchar(45) DEFAULT NULL,
   `Donor_State` varchar(45) DEFAULT NULL,
   `Donor_City` varchar(45) DEFAULT NULL,
   `Donor_Center` varchar(45) DEFAULT NULL,
   `Bank_Acct_Holder_Name` varchar(45) NOT NULL,
+  `Donor_Amount` Double NOT NULL,
   `Bank_Name` varchar(45) DEFAULT NULL,
   `Bank_Branch` varchar(45) DEFAULT NULL,
   `Bank_Acct_Num` varchar(45) NOT NULL,
   `Bank_MICR` decimal(10,0) DEFAULT NULL,
   `Bank_IFSC_Cd` varchar(45) DEFAULT NULL,
   `Bank_Acct_Type` varchar(45) NOT NULL,
-  `Bank_UMRN` varchar(45) DEFAULT NULL,
-  `Bank_Sponsor_Cd` varchar(45) DEFAULT NULL,
-  `Bank_Utility_Cd` varchar(45) DEFAULT NULL,
   `Donation_Start_Dt` date NOT NULL,
   `Donation_End_Dt` date NOT NULL,
   `Donation_Frequency` varchar(45) NOT NULL,
-  `Donation_Debit_Type` varchar(45) DEFAULT NULL,
   `TPPS_Consumer_Code` varchar(45) DEFAULT NULL,
   `Create_Dt` varchar(45) NOT NULL,
-  `Create_By` varchar(45) NOT NULL,
-  `Update_Dt` varchar(45) DEFAULT NULL,
-  `Update_By` varchar(45) DEFAULT NULL,
   `Update_Comments` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`Donor_Details_ID`),
   UNIQUE KEY `Donor_Details_ID_UNIQUE` (`Donor_Details_ID`)
@@ -47,3 +40,59 @@ CREATE TABLE `volunteerdetails` (
   `Comments` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`Volunteer_Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
+ CREATE TABLE `uploaddetails`(
+`Update_Details_ID` int(11) NOT NULL AUTO_INCREMENT ,
+ 
+ `Update_Phone_Num` varchar(45) NOT NULL,
+ 
+ `Update_Name` varchar(45) NOT NULL,
+ 
+ `Update_Email_id` varchar(45) NOT NULL,
+ 
+ `Update_Amount` double NOT NULL,
+
+`Update_Status` varchar(45) DEFAULT NULL,
+
+`Update_Region` varchar(45) DEFAULT NULL,
+
+`Update_State` varchar(45) DEFAULT NULL,
+
+`Update_City` varchar(45) DEFAULT NULL,
+ 
+`Update_Center` varchar(45) DEFAULT NULL,
+ 
+`Update_Bank_Acct_Holder_Name` varchar(45) NOT NULL,
+
+  `Update_Bank_Name` varchar(45) DEFAULT NULL,
+
+ `Update_Bank_Branch` varchar(45) DEFAULT NULL,
+
+`Update_Bank_Acct_Num` varchar(45) NOT NULL,
+
+`Update_Bank_MICR` decimal(10,0) DEFAULT NULL,
+
+`Update_Bank_IFSC_Cd` varchar(45) DEFAULT NULL,
+
+`Update_Bank_Acct_Type` varchar(45) NOT NULL,
+
+`Update_Donation_Start_Dt` date NOT NULL,
+
+  `Update_Donation_End_Dt` date NOT NULL,
+
+`Update_Donation_Frequency` varchar(45) NOT NULL,
+
+`Update_TPPS_Consumer_Code` varchar(45) DEFAULT NULL,
+
+`Update_Create_Dt` varchar(45) DEFAULT NULL,
+
+`Update_Comments` varchar(500) DEFAULT NULL,
+
+ 
+
+  PRIMARY KEY (`Update_Details_ID`),
+
+  UNIQUE KEY `Update_Details_ID_UNIQUE` (`Update_Details_ID`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
