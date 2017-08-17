676a8de0770e6d614caa83eb6d16a9066491c59d
Siddhrans <gururaj.n@siddhrans.com>
2017-08-17 15:55:04 +0530

    upload code modified

--- IshaDonationSystem/src/main/java/com/isha/donation/controller/DownloadController.java
+++ IshaDonationSystem/src/main/java/com/isha/donation/controller/DownloadController.java
@@ -90,11 +90,7 @@ public class DownloadController {
 	String FILE_NAME="C:/xls/isha.xls"; 
 	
 	File file=new File(FILE_NAME);
-	//response.setContentType("application/xls");
-	  
-  //   response.setContentType("application/vnd.ms-excel");
-
-  //  response.setHeader("Content-Disposition", "attachment; filename=" + "isha.xls");
+	 
 	
 	response.setContentType("application/vnd.ms-excel");
 	response.setHeader("Content-disposition", "attachment;filename=isha.xls");
@@ -105,11 +101,7 @@ public class DownloadController {
     HSSFWorkbook workbook=new HSSFWorkbook();
     HSSFSheet sheet=workbook.createSheet("isha");
     
-    /*
-	 XSSFWorkbook workbook = new XSSFWorkbook();
-       XSSFSheet sheet = workbook.createSheet("isha");
-        sheet.autoSizeColumn(1000000);
-    */  
+   
        
        System.out.println("Creating excel");
        
@@ -125,7 +117,7 @@ public class DownloadController {
        int rowcoulumn=0;
      int count=0;
      sheet.setColumnWidth(0, 3000);
-     //sheet.addMergedRegion(new CellRangeAddress(1, 2, 1, 2));
+    
      Row rowheader = sheet.createRow(rowNum++);
      
      Cell cellcolumn = rowheader.createCell(coulumncount);
@@ -217,15 +209,7 @@ public class DownloadController {
       }catch (Exception e) {
 		 e.printStackTrace();
 	}
-      /* try {
-           FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
-           workbook.write(outputStream);
-           workbook.close();
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }*/
+       
 
        return new ResponseEntity("downloaded succesfully ",HttpStatus.OK);
 	
@@ -241,11 +225,7 @@ public class DownloadController {
 	String FILE_NAME="C:/xls/isharemitance.xls"; 
 	
 	File file=new File(FILE_NAME);
-	//response.setContentType("application/xls");
-	  
-  //   response.setContentType("application/vnd.ms-excel");
-
-  //  response.setHeader("Content-Disposition", "attachment; filename=" + "isha.xls");
+	 
 	
 	response.setContentType("application/vnd.ms-excel");
 	response.setHeader("Content-disposition", "attachment;filename=isharemitance.xls");
@@ -256,11 +236,7 @@ public class DownloadController {
     HSSFWorkbook workbook=new HSSFWorkbook();
     HSSFSheet sheet=workbook.createSheet("isha");
     
-    /*
-	 XSSFWorkbook workbook = new XSSFWorkbook();
-       XSSFSheet sheet = workbook.createSheet("isha");
-        sheet.autoSizeColumn(1000000);
-    */  
+    
        
        System.out.println("Creating excel");
        
@@ -276,13 +252,13 @@ public class DownloadController {
        int rowcoulumn=0;
      int count=0;
      sheet.setColumnWidth(0, 3000);
-     //sheet.addMergedRegion(new CellRangeAddress(1, 2, 1, 2));
+     
      Row rowheader = sheet.createRow(rowNum++);
      
      Cell cellcolumn = rowheader.createCell(coulumncount);
      CellStyle style = workbook.createCellStyle();
      style.setFillBackgroundColor(IndexedColors.YELLOW .getIndex());
-     //style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+     
      style.setBorderBottom(CellStyle.BORDER_THIN);
      style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
      style.setBorderLeft(CellStyle.BORDER_THIN);
@@ -368,15 +344,7 @@ public class DownloadController {
       }catch (Exception e) {
 		 e.printStackTrace();
 	}
-      /* try {
-           FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
-           workbook.write(outputStream);
-           workbook.close();
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }*/
+  
 
        return new ResponseEntity("downloaded succesfully ",HttpStatus.OK);
 	
@@ -427,12 +395,7 @@ public class DownloadController {
 			    	}else{
 			    		return new ResponseEntity("need  BRANCHNAME",HttpStatus.CONFLICT);
 			    	}
-			    	/*st=row.getCell(cell++).getStringCellValue();
-			    	if(st.equals("BRANCHNAME")){
-			    		
-			    	}else{
-			    		return new ResponseEntity("need  BRANCHNAME",HttpStatus.CONFLICT);
-			    	}*/
+			    	 
 			    	
 			    	st=row.getCell(cell++).getStringCellValue();
 			    	System.out.println(st);
@@ -484,14 +447,7 @@ public class DownloadController {
 			    		return new ResponseEntity("need  STARTDATE",HttpStatus.CONFLICT);
 			    	}
 			    	
-			    	
-			    	/*st=row.getCell(cell++).getStringCellValue();
-			    	if(st.equals("STARTDATE")){
-			    		
-			    	}else{
-			    		return new ResponseEntity("need  STARTDATE",HttpStatus.CONFLICT);
-			    	}*/
-			    	
+			     
 			    	st=row.getCell(cell++).getStringCellValue();
 			    	System.out.println(st);
 			    	if(st.equals("ENDDATE")){
@@ -544,70 +500,54 @@ public class DownloadController {
 			    	
 			    	System.out.println("branchname"+row.getCell(cell).getStringCellValue());
 			    	uploaddonor.setBranchName(row.getCell(cell++).getStringCellValue());
-			    	
-			    	
-			    	//double ss=row.getCell(cell++).getStringCellValue();
-			    	long d=(long)row.getCell(cell++).getNumericCellValue();
-			    	System.out.println("moble"+d);
-			    	String  number=String.valueOf(d);
-			    	System.out.println(number);
+			   
 			    	 
 			    	
+			    	int accnum=(int)row.getCell(cell++).getNumericCellValue();
+			    	String accnumber=String.valueOf(accnum);
 			    	
-			    	//String st=row.getCell(cell).getStringCellValue();
 			     
-			    	// System.out.println("accountnumber"+st);
-			    	uploaddonor.setBankAccountNumber(number);
+			    	uploaddonor.setBankAccountNumber(accnumber);
+			   
 			    	
-			    	//System.out.println(row.getCell(cell).getStringCellValue());
-			    	/*String d=row.getCell(cell).getStringCellValue();
-			    	System.out.println(d);*/
-
 			    	
+			     
 			    	 
 			    	
-			    	System.out.println("micr"+row.getCell(cell).getNumericCellValue());
+			    	System.out.println((int)row.getCell(cell).getNumericCellValue());
 			    	uploaddonor.setBankMICR((int)row.getCell(cell++).getNumericCellValue());
 			    	
-			    	d=(long)row.getCell(cell++).getNumericCellValue();
-			    	System.out.println("moble"+d);
-			    	number=String.valueOf(d);
-			    	System.out.println(number);
+			    	 
 			    	
-			    	//System.out.println("ifscode"+row.getCell(cell).getStringCellValue());
-			    	uploaddonor.setBankIfscCode(number);
+			    	
+			    	
+			    	
+			    	
+			    	 
+			    	
+			    	 
+			    	String ifsc=row.getCell(cell++).getStringCellValue();
+			    	System.out.println("ifs"+ifsc);
+			    	uploaddonor.setBankIfscCode(ifsc);
+			    	
+			  
 			    	
 			    	System.out.println("accounttype"+row.getCell(cell).getStringCellValue());
 			    	uploaddonor.setBankAccountType(row.getCell(cell++).getStringCellValue());
-			    	
+			    	 
 			    	System.out.println("email"+row.getCell(cell).getStringCellValue());
 			    	uploaddonor.setEmail(row.getCell(cell++).getStringCellValue());
 			    	
-			    	/*  String d=(String)row.getCell(cell++).getStringCellValue();
-				     int dd=Integer.parseInt(d);
-				     System.out.println(dd);*/
-			    	
-			    	  
-			    	  d=(long)row.getCell(cell++).getNumericCellValue();
-			    	System.out.println("moble"+d);
-			    	String mb=String.valueOf(d);
 			    	 
 			    	
-			    	//System.out.println("mobile"+String.valueOf((row.getCell(cell).getStringCellValue())));
-			    	 uploaddonor.setUpdatemobileNumber(mb);
 			    	 
 			    	 
-			    	// System.out.println(row.getCell(cell).getStringCellValue());
-			    //	uploaddonor.setUpdatemobileNumber(row.getCell(cell++).getStringCellValue());
-			    	//System.out.println(row.getCell(cell).getStringCellValue());
-			    	 /*
-			    	  Date date=row.getCell(cell).getDateCellValue();
-			    	   String ddte=String.valueOf(date);
-			    	 DateFormat cstDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
-			    	 System.out.println(cstDate); 
-			    	 */
+			    long mobile=(long)row.getCell(cell++).getNumericCellValue();
+			    	String mob=String.valueOf(mobile);
+			    	System.out.println("mobile"+mob);
+			    	uploaddonor.setUpdatemobileNumber(mob);
 			    	 
-			    	 
+			    	
 			    	 System.out.println("startdate"+row.getCell(cell).getDateCellValue());
 			    	uploaddonor.setStartDate((Date)row.getCell(cell++).getDateCellValue());
 			    	
@@ -617,25 +557,14 @@ public class DownloadController {
 			    	
 			    	uploaddonor.setEndDate((Date)row.getCell(cell++).getDateCellValue());
 			    	
-			    	 
-			    	/*String amt=row.getCell(cell++).getStringCellValue();
-			    	System.out.println(amt);*/
+			    	  
 			    	System.out.println("amoutn"+row.getCell(cell).getNumericCellValue());
 			    	  	uploaddonor.setAmount((double)row.getCell(cell++).getNumericCellValue());
 			    	  	
 			    	   
-				    	//System.out.println(row.getCell(cell).getNumericCellValue());
-			    	  	
-			    	 //System.out.println(row.getCell(cell).getStringCellValue());
+				     
 			    	uploaddonor.setFrequency(row.getCell(cell++).getStringCellValue());
-			    	//System.out.println(row.getCell(cell).getStringCellValue());
-			    	/*
-			    	uploaddonor.setStatus(row.getCell(cell++).getStringCellValue());
-			    	uploaddonor.setRegion(row.getCell(cell++).getStringCellValue());
-			    	uploaddonor.setState(row.getCell(cell++).getStringCellValue());
-			    	uploaddonor.setCity(row.getCell(cell++).getStringCellValue());
-			    	uploaddonor.setCenter(row.getCell(cell++).getStringCellValue());
-			    	
+			    	 
 			    	
 			    	
 			    	
@@ -647,7 +576,7 @@ public class DownloadController {
 			    	
 			    	uploaddonor.setTppsConsumerCode(row.getCell(cell++).getStringCellValue());
 			    	uploaddonor.setCreateDate(row.getCell(cell++).getStringCellValue());
-			    	uploaddonor.setCreateDate(row.getCell(cell++).getStringCellValue());*/
+			    	uploaddonor.setCreateDate(row.getCell(cell++).getStringCellValue()); 
 			    	
 			  try{	
 				  System.out.println("uploadmobile number->"+uploaddonor.getUpdatemobileNumber());
--- SQL Script/idsdb.sql
+++ SQL Script/idsdb.sql
@@ -1,33 +1,32 @@
 CREATE DATABASE `idsdb`;
  
 CREATE TABLE `donordetails` (
-  `Donor_Details_ID` int(11) NOT NULL AUTO_INCREMENT ,
- `Donor_Phone_Num` varchar(45) NOT NULL,
- `Donor_Name` varchar(45) NOT NULL,
- `Donor_Email_id` varchar(45) NOT NULL,
-  `Donor_Status` varchar(45) DEFAULT NULL,
- `Donor_Region` varchar(45) DEFAULT NULL,
- `Donor_State` varchar(45) DEFAULT NULL,
- `Donor_City` varchar(45) DEFAULT NULL,
- `Donor_Center` varchar(45) DEFAULT NULL,
- `Bank_Acct_Holder_Name` varchar(45) NOT NULL,
- `Bank_Name` varchar(45) DEFAULT NULL,
- `Bank_Branch` varchar(45) DEFAULT NULL,
- `Bank_Acct_Num` varchar(45) NOT NULL,
- `Bank_MICR` decimal(10,0) DEFAULT NULL,
- `Bank_IFSC_Cd` varchar(45) DEFAULT NULL,
- `Bank_Acct_Type` varchar(45) NOT NULL,
- `Donation_Start_Dt` date NOT NULL,
- `Donation_End_Dt` date NOT NULL,
- `amount` double not null,
- `Donation_Frequency` varchar(45) NOT NULL,
- `TPPS_Consumer_Code` varchar(45) DEFAULT NULL,
- `Create_Dt` varchar(45) NOT NULL,
- `Update_Comments` varchar(500) DEFAULT NULL,
- PRIMARY KEY (`Donor_Details_ID`),
- UNIQUE KEY `Donor_Details_ID_UNIQUE` (`Donor_Details_ID`)
+   `Donor_Details_ID` int(11) NOT NULL AUTO_INCREMENT ,
+  `Donor_Phone_Num` varchar(45) NOT NULL,
+  `Donor_Name` varchar(45) NOT NULL,
+  `Donor_Email_id` varchar(45) NOT NULL,
+   `Donor_Status` varchar(45) DEFAULT NULL,
+  `Donor_Region` varchar(45) DEFAULT NULL,
+  `Donor_State` varchar(45) DEFAULT NULL,
+  `Donor_City` varchar(45) DEFAULT NULL,
+  `Donor_Center` varchar(45) DEFAULT NULL,
+  `Bank_Acct_Holder_Name` varchar(45) NOT NULL,
+  `Donor_Amount` Double NOT NULL,
+  `Bank_Name` varchar(45) DEFAULT NULL,
+  `Bank_Branch` varchar(45) DEFAULT NULL,
+  `Bank_Acct_Num` varchar(45) NOT NULL,
+  `Bank_MICR` decimal(10,0) DEFAULT NULL,
+  `Bank_IFSC_Cd` varchar(45) DEFAULT NULL,
+  `Bank_Acct_Type` varchar(45) NOT NULL,
+  `Donation_Start_Dt` date NOT NULL,
+  `Donation_End_Dt` date NOT NULL,
+  `Donation_Frequency` varchar(45) NOT NULL,
+  `TPPS_Consumer_Code` varchar(45) DEFAULT NULL,
+  `Create_Dt` varchar(45) NOT NULL,
+  `Update_Comments` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`Donor_Details_ID`),
+  UNIQUE KEY `Donor_Details_ID_UNIQUE` (`Donor_Details_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `volunteerdetails` (
   `Volunteer_Id` int(11) NOT NULL AUTO_INCREMENT,
   `Volunteer_User_Name` varchar(45) NOT NULL,
@@ -41,3 +40,61 @@ CREATE TABLE `volunteerdetails` (
   `Comments` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`Volunteer_Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
+    CREATE TABLE `uploaddetails`(
+
+`Update_Details_ID` int(11) NOT NULL AUTO_INCREMENT ,
+  
+`Update_Phone_Num` varchar(45) NOT NULL,
+ 
+ `Update_Name` varchar(45) NOT NULL,
+ 
+ `Update_Email_id` varchar(45) NOT NULL,
+ 
+ `Update_Amount` double NOT NULL,
+
+`Update_Status` varchar(45) DEFAULT NULL,
+
+`Update_Region` varchar(45) DEFAULT NULL,
+
+`Update_State` varchar(45) DEFAULT NULL,
+
+`Update_City` varchar(45) DEFAULT NULL,
+ 
+`Update_Center` varchar(45) DEFAULT NULL,
+
+ `Update_Bank_Acct_Holder_Name` varchar(45) NOT NULL,
+ 
+ `Update_Bank_Name` varchar(45) DEFAULT NULL,
+
+ `Update_Bank_Branch` varchar(45) DEFAULT NULL,
+
+`Update_Bank_Acct_Num` varchar(45) NOT NULL,
+
+`Update_Bank_MICR` decimal(10,0) DEFAULT NULL,
+
+`Update_Bank_IFSC_Cd` varchar(45) DEFAULT NULL,
+
+`Update_Bank_Acct_Type` varchar(45) NOT NULL,
+
+`Update_Donation_Start_Dt` date NOT NULL,
+ 
+ `Update_Donation_End_Dt` date NOT NULL,
+
+`Update_Donation_Frequency` varchar(45) NOT NULL,
+
+`Update_TPPS_Consumer_Code` varchar(45) DEFAULT NULL,
+
+`Update_Create_Dt` varchar(45) DEFAULT NULL,
+
+`Update_Comments` varchar(500) DEFAULT NULL,
+
+ 
+
+  PRIMARY KEY (`Update_Details_ID`),
+ 
+ UNIQUE KEY `Update_Details_ID_UNIQUE` (`Update_Details_ID`)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
